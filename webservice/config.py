3d669954141d919ae2a23c9306849d0aed2473e5
Anna Hmyria <anyagmyrka@gmail.com>
2022-01-16 23:07:09 +0200

    EDIT: html files

--- webservice/config.py
+++ webservice/config.py
@@ -4,4 +4,4 @@ import os
 class Config(object):
     TESTING = False
     SECRET_KEY = os.urandom(32)
-    DATABASE_URI = "sqlite:///".join(os.environ.get("DATABASE_URI"))
+    DATABASE_URI = "sqlite:///".join(os.environ.get("DATABASE_URI", "workdb.db"))
